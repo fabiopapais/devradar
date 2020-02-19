025f4d3dc19a1506a4a050b83e1aeed827b521da
Fábio Papais <fabiopapais0@gmail.com>
2020-02-19 22:22:01 +0000

    delete mobile

--- mobile/src/pages/Main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { View } from 'react-native'
-
-function Main() {
-    return <View />
-}
-
-export default Main
--- mobile/src/pages/Profile.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { View } from 'react-native'
-
-function Profile() {
-    return <View />
-}
-
-export default Profile
